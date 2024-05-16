3d90bf5a1ab27db9072df548cb3d9ce0df816fae
Jose de la Ossa C <joseamdresdelaossa@gmail.com>
2024-05-16 18:34:22 -0500

    Creacion de profile, logout y login

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -36,5 +36,52 @@ export const registro = async(req, res) => {
 }
 
 export const loguear = async(req, res) => {
-    
+    const {email, password} = req.body
+
+    try {
+        //guardamos en la variable el usuario o email que encontramos en la base de datos
+        const userFound = await User.findOne({email})
+        //HAcemos una validacion
+        if(!userFound) return res.status(400).json(["Usuario no encontrado"])
+
+        //logica para verificar el password del usuario con el password de la base de datos del email que encontramos
+        const isMatch = await bcrypt.compare(password, userFound.password)
+        //hacemos una validacion
+        if(isMatch) return res.status(400).json(["Contraseña incorrecta"])
+
+        //utilizamos el token
+        const token = await createAccessToken({id: userFound._id}) //enviamos el id para que lo cree como un token 
+        //creamos una cookie que ya viene de express, para que se cree diractamente la cookie en nuestro navegador 
+        res.cookie("token", token)
+
+        //respuesta al cliente
+        res.json({
+            email:userFound.email,
+            username: userFound.username,
+            id: userFound.id, 
+            createAt: userFound.createdAt
+        })
+
+    } catch (error) {
+        res.status(400).json({message:error.message})
+    }
+}
+
+export const logout = (res, req) => {
+    res.cookie("token", "",{
+        expires: new Date(0),
+    });
+    return res.sendStatus(200)
+}
+
+export const profile = async (req, res) => {
+    const usuarioEncontrado = await User.findById(req.user.id)
+
+    if(!usuarioEncontrado) return res.status(400).json(["Usuario encontrado"]);
+
+    return res.json({
+        id: usuarioEncontrado._id,
+        username: usuarioEncontrado.username,
+        email: usuarioEncontrado.email
+    })
 }
